3a071b75742f5883204944b131d91cb222533cf2
Samyuth <samyuth.sagi@gmail.com>
2024-04-28 15:55:01 -0400

    Cleaned up contract code and changed oracle interaction

--- contracts/BlockBiometrics.sol
+++ contracts/BlockBiometrics.sol
@@ -2,6 +2,14 @@
 pragma solidity ^0.8.0;
 
 //import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "./Oracle.sol";
+
+struct Request {
+    address sender;
+    uint256 authenticationRequestId;
+    uint256 requestTime;
+    string result;
+}
 
 contract BlockBiometrics {
     //using SafeMath for uint256;
@@ -9,12 +17,11 @@ contract BlockBiometrics {
     struct Visitor {
         bool isRegistered;
         bool hasAccess;
-        uint256 authenticationRequestId; // ID of the authentication request
-        uint256 requestTime; // Time of the authentication request
+        mapping(uint256 => Request) requests;
     }
 
     address public owner;
-    address public oracle;
+    Oracle public oracle;
     mapping(address => Visitor) public visitors;
     mapping(uint256 => address) public authenticationRequests; // Mapping from request ID to visitor address
 
@@ -47,59 +54,47 @@ contract BlockBiometrics {
         require(!visitors[msg.sender].hasAccess, "Visitor already has access");
         _;
     }
-    function getIsRegistered() view public returns (bool) {
-        return visitors[msg.sender].isRegistered;
-    }
-    function getRequestID() view public returns (uint256) {
-        return visitors[msg.sender].authenticationRequestId;
-    }
-    function getFromRequestId(uint256 id) view public returns (address) {
-        return authenticationRequests[id];
-    }
-    function getAccess() view public returns (bool) {
-        return visitors[msg.sender].hasAccess;
-    }
     
     constructor() {
         owner = msg.sender;
+        oracle = new Oracle(owner);
     }
-    function setOracle(address oracle_) external {
-        oracle = oracle_;
-    }
+
     function register() external notRegistered {
         visitors[msg.sender].isRegistered = true;
         emit VisitorRegistered(msg.sender);
     }
 
-    function requestAccess() external registered hasNoAccess {
+    function authenticate() external registered hasNoAccess {
+        // Construct a request
         uint256 requestId = generateRequestId();
-        visitors[msg.sender].authenticationRequestId = requestId; // Link request ID to visitor
-        visitors[msg.sender].requestTime = block.timestamp; // Record request time
-        authenticationRequests[requestId] = msg.sender; // Store request ID
-        (Oracle)(oracle).makeRequest("placeholder", requestId);
+        visitors[msg.sender].requests[requestId] = Request(
+            address(this), requestId, block.timestamp, ""
+        );
+
+        // Store request ID
+        authenticationRequests[requestId] = msg.sender;
+
+        // Send request to the oracle
+        (oracle).makeRequest(visitors[msg.sender].requests[requestId]);
+
+        // Emit event
         emit AccessRequested(msg.sender, requestId); // Emit event with request ID
     }
 
-    function authenticateRequest() external registered hasNoAccess {
-        uint256 requestId = generateRequestId();
-        visitors[msg.sender].authenticationRequestId = requestId; // Link request ID to visitor
-        visitors[msg.sender].requestTime = block.timestamp; // Record request time
-        authenticationRequests[requestId] = msg.sender; // Store request ID
-        emit AuthenticateRequest(msg.sender, requestId); // Emit event with request ID
-    }
 
-    function receiveConfirmation(uint256 requestId, string memory result) external {
-        address visitor = authenticationRequests[requestId]; // Get visitor address from request ID
+    function receiveResponse(uint256 requestId, string memory result) external {
+        // Get visitor address from request ID and make sure the ID and visitor is valid
+        address visitor = authenticationRequests[requestId]; 
+        require(visitor != address(0), "Invalid request ID");
         require(visitors[visitor].isRegistered, "Visitor is not registered");
-        require(visitors[visitor].authenticationRequestId == requestId, "Invalid request ID");
-        // This is the fallback
-        require(block.timestamp - visitors[visitor].requestTime <= timeoutDuration, "Request timed out");
-        // Implement logic to receive confirmation from the oracle
-        // For demonstration purposes, let's assume the oracle confirms access for the visitor
-        // Replace this with the actual oracle integration logic
-        // For simplicity, we'll just emit an event here
-        emit AccessGranted(visitor);
+
+        // Get the visitors response
+        visitors[visitor].requests[requestId].result = result;
+
+        // Visitor now has access
         visitors[visitor].hasAccess = true;
+        emit AccessGranted(visitor);
     }
 
     function revokeAccess(address visitor) external onlyOwner {
@@ -108,46 +103,14 @@ contract BlockBiometrics {
         emit AccessRevoked(visitor);
     }
 
-    function accessHome() external view returns (string memory) {
+    function accessHome() external view returns (bool) {
         require(visitors[msg.sender].hasAccess, "Visitor does not have access");
-        return "Home access granted";
+        return true;
     }
 
     // Function to generate unique request ID
     function generateRequestId() private returns (uint256) {
         authenticationRequestCounter = authenticationRequestCounter + 1;
         return authenticationRequestCounter;
-    }
-}
-contract Oracle {
-    address public owner;
-    string public request;
-    string public result;
-    address public sender;
-    uint256 public id;
-
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Only the contract owner can call this function");
-        _;
-    }
-
-    constructor() {
-        owner = msg.sender;
-    }
-    
-    function makeRequest(string memory _request, uint256 sender_id) external{
-        request = _request;
-        sender = msg.sender;
-        id = sender_id;
-    }
-    
-    function getRequest() public view onlyOwner returns (string memory) {
-        return request;
-    }
-    
-    function returnResult(string memory _result) public onlyOwner {
-        result = _result;
-        // call original_contract method
-        (BlockBiometrics)(sender).receiveConfirmation(id, result);
     }
 }
---
+++ contracts/Oracle.sol
@@ -0,0 +1,32 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./BlockBiometrics.sol";
+
+contract Oracle {
+    address public owner;
+    Request[] public request_queue;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the contract owner can call this function");
+        _;
+    }
+
+    constructor(address _owner) {
+        owner = _owner;
+    }
+    
+    function makeRequest(Request memory _request) external{
+        request_queue.push(_request);
+    }
+
+    function popRequest() onlyOwner external returns(Request memory) {
+        Request memory r = request_queue[request_queue.length-1];
+        request_queue.pop();
+        return r;
+    }
+    
+    function publishResult(address _sender, uint256 _id, string memory _result) public onlyOwner {
+        (BlockBiometrics)(_sender).receiveResponse(_id, _result);
+    }
+}
